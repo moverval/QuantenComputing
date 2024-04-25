e975491634143a05c01b4674a5ddf88ba6826486
Moritz <moritz@voulter.com>
2024-04-25 23:38:30 +0200

    Fix main

--- main.jl
+++ main.jl
@@ -2,12 +2,13 @@ include("core.jl")
 include("constants.jl")
 include("separability.jl")
 include("control.jl")
+include("bloch.jl")
 
 import .Separability.separable, .Separability.separate, .Separability.findlambda
 import .Control.control, .Control.cntl
 
 # Ein normales CNOT ist das gleiche wie ein kontrolliertes rX Gatter
-CNOT = cntl(2, 1, 2, rX)
+CNOT = cntl(2, 1, 2, Px)
 
 # Ein CNOT Gatter welches Q-Bit 2 für die Aktivierung und 1 für die Manipulation nutzt.
-CNOT2to1 = cntl(2, 2, 1, rX)
+CNOT2to1 = cntl(2, 2, 1, Px)
