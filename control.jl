9eab1d537c54b4f32956f4e4b37734418277765a
Moritz <moritz@voulter.com>
2024-04-26 13:18:14 +0200

    Cleanup

--- control.jl
+++ control.jl
@@ -1,29 +1,10 @@
 module Control
     include("core.jl")
     include("constants.jl")
+    include("notation.jl")
+
+    import .Notation.bitchain
     export cntl, control
-
-    bitchain(input::Vector, x::Int)::Vector{Bool} = begin
-        ex = floor(log(x)/log(2))
-
-        if ex == -Inf
-            return input
-        end
-
-        ex = convert(Int, ex)
-
-        for e in ex:-1:0
-            num = convert(Int, 2^e)
-            if x - num >= 0
-                x -= num
-                input[e + 1] = true
-            else
-                input[e + 1] = false
-            end
-        end
-
-        return input
-    end
 
     # qb: Q-Bits
     # from: Index der Aktivierung
@@ -34,17 +15,15 @@ module Control
     #
     # Beispiel: cntl(2, 1, 2, pX) == CNOT
     # Beispiel: cntl(2, 1, 2, RY(pi/2)) * (k0 ⊗ k1) == k0 ⊗ k1
-    # Beispiel: cntl(2, 1, 2, RY(pi/2)) * (k1 ⊗ k1) == k1 ⊗ (RY(pi/2)*k1)
     cntl = control(qb, from, to, mat) = begin
         result = zeros(2 ^ qb, 2 ^ qb)
 
         for i in 0:(2^qb-1)
             chain = bitchain(zeros(qb), i)
-            chain = reverse(chain)
             vec = [1]
 
             for (x, c) in enumerate(chain)
-                if chain[from] && to == x
+                if chain[from] != 0 && to == x
                     if c
                         vec = vec ⊗ (mat * k1)
                     else
--- main.jl
+++ main.jl
@@ -1,5 +1,6 @@
 include("core.jl")
 include("constants.jl")
+include("notation.jl")
 include("separate.jl")
 include("control.jl")
 include("bloch.jl")
@@ -7,9 +8,17 @@ include("latex.jl")
 
 import .Separate.separable, .Separate.separate, .Separate.findlambda, .Separate.rs
 import .Control.control, .Control.cntl
+import .Notation.ket
 
 # Ein normales CNOT ist das gleiche wie ein kontrolliertes rX Gatter
 CNOT = cntl(2, 1, 2, Px)
 
 # Ein CNOT Gatter welches Q-Bit 2 für die Aktivierung und 1 für die Manipulation nutzt.
 CNOT2to1 = cntl(2, 2, 1, Px)
+
+S = [
+    1 0 0 0;
+    0 0 1 0;
+    0 1 0 0;
+    0 0 0 1
+]
